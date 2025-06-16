0868bf54c6d74540f5dc1ed78dcad92103550ad6
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-16 23:48:48 +0300

    installing dependancies in the backend and fontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello</h1>
+     <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
