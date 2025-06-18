45fbde99cc4fc9d3be627c4f742612b7429d8dd0
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-19 00:39:59 +0300

    left sidebar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { Outlet } from "react-router-dom"
+import LeftSidebar from "./components/LeftSidebar"
 
 
 const MainLayout = () => {
@@ -10,7 +11,7 @@ const MainLayout = () => {
 <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
     {/* =====LEFT SIDEBAR===== */}
     <ResizablePanel defaultSize={20} minSize={isMobile? 0 : 10} maxSize={30}>
-            left sidebar
+            <LeftSidebar/>
     </ResizablePanel>
     <ResizableHandle className="w-2 bg-black rounded-lg transition-colors"/>
     {/* =====MAIN CONTENT===== */}
