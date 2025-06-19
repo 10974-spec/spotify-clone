65077dda4c87d0f0db4a307cac4fa12c2c267abe
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-19 21:19:36 +0300

    albums page

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -7,7 +7,9 @@ interface MusicStore{
     albums: Album[],
     isLoading: boolean,
     error: null | string,
-    fetchAlbums: () => Promise<void>;
+    currentAlbum : null | Album,
+    fetchAlbums: () => Promise<void>,
+    fetchAlbumById: (id: string) => Promise<void>
 }
 
 export const useMusicStore = create<MusicStore>((set)=>({
@@ -15,6 +17,7 @@ export const useMusicStore = create<MusicStore>((set)=>({
  songs: [],
  isLoading: false,
  error: null,
+ currentAlbum : null,
 
  fetchAlbums: async () => {
    // data fetch logic
@@ -30,5 +33,20 @@ export const useMusicStore = create<MusicStore>((set)=>({
    }finally{
     set({isLoading: false})
    }
+ },
+
+ fetchAlbumById: async (id) => {
+    set({
+        isLoading: true,
+        error: null,
+       })
+       try {
+        const response = await axiosInstance.get(`/albums/${id}`)
+        set({currentAlbum: response.data})
+       }catch (error:any){
+        set({error: error.response.data.message})
+       }finally{
+        set({isLoading: false})
+       }
  }
 }))
