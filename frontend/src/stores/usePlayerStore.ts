c6d16042f8dce992d37626f4773d47e8034a2eab
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-21 12:43:26 +0300

    play song functionality

--- frontend/src/stores/usePlayerStore.ts
+++ frontend/src/stores/usePlayerStore.ts
@@ -1,33 +1,47 @@
 import type { Song } from "@/types";
-import {create } from "zustand";
-
-
+import { create } from "zustand";
 
 interface PlayerStore {
-    currentSong: Song | null;
-    isPlaying: boolean;
-    queue: Song[];
-    currentIndex: number;
+  currentSong: Song | null;
+  isPlaying: boolean;
+  queue: Song[];
+  currentIndex: number;
 
-    initializeQueue: (songs: Song[]) => void;
-    playAlbum: (songs: Song[],startIndex?: number) => void;
-    setCurrentSong: (song: Song | null ) => void;
-    togglePlay: () => void;
-    playPrevious: () => void;
-    playNext: () => void;
+  initializeQueue: (songs: Song[]) => void;
+  playAlbum: (songs: Song[], startIndex?: number) => void;
+  setCurrentSong: (song: Song | null) => void;
+  togglePlay: () => void;
+  playPrevious: () => void;
+  playNext: () => void;
 }
 
-export const usePlayerStore = create<PlayerStore>((set,get)=> ({
-    currentSong: null,
-    isPlaying: false,
-    queue: [],
-    currentIndex: -1,
+export const usePlayerStore = create<PlayerStore>((set, get) => ({
+  currentSong: null,
+  isPlaying: false,
+  queue: [],
+  currentIndex: -1,
 
+  initializeQueue: (songs) => {
+    set({
+      queue: songs,
+      currentSong: get().currentSong || songs[0],
+      currentIndex: get().currentIndex === -1 ? 0 : get().currentIndex,
+    });
+  },
+  playAlbum: (songs: Song[], startIndex = 0) => {
+    if (songs.length === 0) return;
 
-    initializeQueue: (songs) =>{},
-   playAlbum: (songs: Song[], startIndex?: number) => {},
-   setCurrentSong: (song: Song | null) => {},
-   togglePlay: () => {},
-   playNext: () => {},
-   playPrevious: () => {},
-    }))
+    const song = songs[startIndex];
+
+    set({
+      queue: songs,
+      currentSong: song,
+      currentIndex: startIndex,
+      isPlaying: true,
+    });
+  },
+  setCurrentSong: (song: Song | null) => {},
+  togglePlay: () => {},
+  playNext: () => {},
+  playPrevious: () => {},
+}));
